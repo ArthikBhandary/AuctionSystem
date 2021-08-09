acebf7134d8dc705b065d7af9c0109111d4b3402
Marchingspy <63202910+Marchingspy@users.noreply.github.com>
2021-08-09 13:26:49 +0530

    Threads

--- src/application/controllers/interfaces/DynamicItemScrollController.java
+++ src/application/controllers/interfaces/DynamicItemScrollController.java
@@ -3,7 +3,6 @@ package application.controllers.interfaces;
 import application.controllers.NextPageController;
 import application.models.Item;
 import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 
@@ -13,26 +12,31 @@ import java.util.ArrayList;
  * Abstract class to define a controller to scene which has a scroll pane which displays a list of objects
  */
 public abstract class DynamicItemScrollController extends NextPageController {
-    public void initialize(){
-        Platform.runLater(this::scrollPaneUpdate);
+    public void initialize() {
+        scrollPaneUpdate();
     }
 
-    void scrollPaneUpdate(){
+    void scrollPaneUpdate() {
         VBox content = new VBox();
         ArrayList<Item> itemArrayList = Item.getObjectList();
-        for(Item item: itemArrayList){
-            System.out.println();
-            if(checkItem(item)){
-                content.getChildren().add(getItemNode(item));
-            }
+        for (Item item : itemArrayList) {
+            new Thread(() -> {
+                if (checkItem(item)) {
+                    Platform.runLater(() -> {
+                        content.getChildren().add(getItemNode(item));
+                    });
+                }
+            }).start();
+
         }
         content.setSpacing(50);
         getScrollPane().setContent(content);
     }
 
-    protected boolean checkItem(Item item){
+    protected boolean checkItem(Item item) {
         return true;
     }
+
     abstract protected ScrollPane getScrollPane();
 
     abstract protected VBox getItemNode(Item item);
